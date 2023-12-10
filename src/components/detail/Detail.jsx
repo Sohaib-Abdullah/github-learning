90300c9647421d76543fb8d5cad0e4e468f43973
Sohaib <sohaibsohaib77@gmail.com>
2023-12-10 15:50:18 +0500

    school-detail

---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Detail = () => {
+  return (
+    <div>
+      This is school is build in 1905. It is very old school. Many students got
+      education from it
+    </div>
+  );
+};
+
+export default Detail;

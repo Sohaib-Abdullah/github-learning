a44b6bc004021942e9d35b7b30495fc658ca5737
Sohaib <sohaibsohaib77@gmail.com>
2023-12-10 15:56:57 +0500

    school-info

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Detail from "./components/detail/Detail";
+import Hero from "./components/hero/Hero";
+import Info from "./components/info/Info";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Detail />
+      <Info/>
     </div>
   );
 }

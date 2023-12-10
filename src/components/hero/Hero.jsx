b03ccd201a5282f04d6c21b4d3215e77c8d318a8
Sohaib <sohaibsohaib77@gmail.com>
2023-12-10 15:14:06 +0500

    hero-section-added

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        Government High School Buch Multan. It is located at Khokharan Shujabad Road.
+    </div>
+  )
+}
+
+export default Hero

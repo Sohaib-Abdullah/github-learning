5d64c84adc84be1361700a8d693310ab8cfc0813
Sohaib <sohaibsohaib77@gmail.com>
2023-12-10 16:11:12 +0500

    student-info

---
+++ src/components/student/Student.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Student = () => {
+  return (
+    <div>
+      Many students study in this school.Currently 432 students are enrolled in
+      this school
+    </div>
+  );
+};
+
+export default Student;

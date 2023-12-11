4bc2af0d2d6ef7f05dae185670a0d883eedb3d7e
Sohaib <sohaibsohaib77@gmail.com>
2023-12-11 16:26:26 +0500

    test

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,13 +8,10 @@ const Navbar = () => {
       </h1>
       <h1>Rise College</h1>
       <h1>Admire College</h1>
-      <h1>Affan Khan</h1>
+      <h1>Affan Sohaib</h1>
+      <h1>Affan Sohaib</h1>
       <h1>Shehram Khan</h1>
       <h1>Shehram Khan</h1>
-      <h1>Shehram Khan</h1>
-      <h1>Waseem Akram</h1>
-      <h1>Waseem Akram</h1>
-      <h1>Waseem Akram</h1>
       <h1>Waseem Akram</h1>
       <h1>Waseem Akram</h1>
     </div>
